92b3018be4faa342f41d355f79f9aecfcdfd543e
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-06-14 13:02:13 -0600

    adds advanced operations

--- calc.js
+++ calc.js
@@ -33,8 +33,21 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === "√"){
 		console.log(Math.sqrt(num1));
 	}
+	if (mathSymbol === "^2"){
+		console.log(num1*num1);
+	}
+	if (mathSymbol === "^3") {
+		console.log(num1*num1*num1);
+	}
+	if (mathSymbol === "^"){
+		console.log(Math.pow(num1, num2));
+	}
+	if (mathSymbol === "%"){
+		console.log(num1%num2);
+	}
 	// This line closes the connection to the command line interface.
 	reader.close()
+	
 
 });
 
